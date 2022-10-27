1800eb525b784e665fcef1048dc8e0d9fe39bba5
LukasBusse <busse.l.95@gmail.com>
2022-10-27 13:28:35 +0200

    Anfang Übung 4

---
+++ uebungen/4/basic-functions.js
@@ -0,0 +1,33 @@
+/** Aufgabe 1 */
+function identity(x) {
+    return x;
+}
+function identity_function(x) {
+    return function(x) {
+        return x;
+    }
+}
+function add(x,y) {
+    return x+y;
+}
+function mul(x,y) {
+    return x*y;
+}
+function addf(x) {
+    return function(y) {
+        return x+y;
+    }
+}
+function applyf(f) {
+    return f(x) {
+        return f(y);
+    }
+}
+
+/** Aufgabe 2 */
+
+/** Aufgabe 3 */
+function fibo() {
+
+}
+/** Aufgabe 4 */
