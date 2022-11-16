ef2a255531d50b77743d2ff2a069a92fdd690d53
LukasBusse <busse.l.95@gmail.com>
2022-11-16 17:42:54 +0100

    buttons überarbeitet

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 //Refresh, falls später mehrere Lifecycles notwendig werden
 const refresh = () => {
     /**Übungs-Buttons generieren: */
-    getUebungenButtons();
+    getButtons('header', 10);
+    getButtons('main', 3);
+    
 }
 
 /**Übungs-Buttons generieren */
-const getUebungenButtons = () => {
-    const navbar = document.querySelector( "header nav ul" );
-    // Noch keine Möglichkeit die Anzahl der Übungen zu zählen, daher hardcore-Loop
-    for (let i=0; i<10; i++) {
+const getButtons = ( htmlclass, amount ) => {
+    const navbar = document.querySelector( `${htmlclass} nav ul` );
+
+
+    // Noch keine Möglichkeit die Anzahl der Übungen zu zählen, daher hardcode-Loop
+    for (let i=0; i<amount; i++) {
         const $li = document.createElement('li');
-        $li.innerHTML = `<button class='navbar-button'>Übung ${i+1}</button>`
+        const $button = document.createElement('button');
+        $button.classList='navbar-button';
+        htmlclass ==='header' ? $button.innerHTML = `Übung ${i+1}` : $button.innerHTML = `Aufgabe ${i+1}`;
+        $li.appendChild($button);
         navbar.appendChild($li);
     }
 }
