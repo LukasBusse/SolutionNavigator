625d67e819d0f20b431cecd9508b282a613a497e
LukasBusse <busse.l.95@gmail.com>
2023-01-10 14:33:23 +0100

    Übung 8 Promises beendet

--- uebungen/8/promises.js
+++ uebungen/8/promises.js
@@ -1,15 +1,12 @@
-function getText() {
-    let A, B;
-    fetch("A.txt").then(response => response.text())
-        .then(data => {
-            A = data;
-        });
-    fetch("B.txt").then(response => response.text())
-        .then(data => {
-            B = data;
-        });
-    console.log(A);
-    console.log(B);
-}
+/**Fetch Text A und B, Split bei \n|\r und entferne extra leerzeichen gleichzeitig */
+//Die Ergebnisse werden synchron konkateniert und auf der Konsole ausgegeben
+//Lösung mit Promise.all:
+Promise.all([
+    fetch('A.txt').then(x => x.text()).then(a => a.split(/\n|\r/)).then(a => a.map(string => string.replace("  ", ""))),
+    fetch('B.txt').then(x => x.text()).then(b => b.split(/\n|\r/)).then(a => a.map(string => string.replace("  ", "")))
+  ]).then(([sentencesA, sentencesB]) => {
+    let final =  [sentencesA, sentencesB].reduce((a,b) => a.map((v,i) => v  + b[i]));
+    final = final.join("");
+    console.log(final);
+  });
 
-getText();
