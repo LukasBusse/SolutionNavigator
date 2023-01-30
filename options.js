7b3bf7b58d5dea36ac64bf25e38420aa5730e2c0
LukasBusse <busse.l.95@gmail.com>
2023-01-30 17:24:41 +0100

    options.json durch options.js

---
+++ options.js
@@ -0,0 +1,202 @@
+/**
+ * Export Module für die Optionen
+ */
+
+export const data = 
+{
+    "HTML": {
+      "Textaufgabe": {
+        "path": "uebungen/1/a1-textaufgaben.html",
+        "title": "Textaufgaben Uebung 1",
+        "description": [
+          "Textaufgabe von Uebung 1"
+        ]
+      },
+      "Wireframe": {
+        "path": "uebungen/1/a2-wireframe.html",
+        "title": "Wireframe Uebung 1",
+        "description": [
+          "Wireframe von Uebung 1"
+        ]
+      }
+    },
+    "CSS_1": {
+      "Textaufgabe": {
+        "path": "uebungen/2/a1-textaufgaben.html",
+        "title": "Textaufgabe",
+        "description": [
+          "Textaufgabe von Uebung 2"
+        ]
+      },
+      "Header": {
+        "path": "uebungen/2/a2-header.html",
+        "title": "Sticky-Header",
+        "description": [
+          "Sticky Header von Uebung 2"
+        ]
+      },
+      "Checkbox": {
+        "path": "uebungen/2/a3-checkbox.html",
+        "title": "Checkbox-Hack",
+        "description": [
+          "Checkbox-Hack von Uebung 2"
+        ]
+      },
+      "Wireframe": {
+        "path": "uebungen/2/a4-wireframe.html",
+        "title": "Wireframe Uebung 2",
+        "description": [
+          "Wireframe von Uebung 2"
+        ]
+      }
+    },
+    "CSS_2": {
+      "Flexbox": {
+        "path": "uebungen/3/a1-flexbox.html",
+        "title": "Flexbox",
+        "description": [
+          "Flexbox von Uebung 3"
+        ]
+      },
+      "Grid": {
+        "path": "uebungen/3/a2-grid.html",
+        "title": "Grid",
+        "description": [
+          "Grid von Uebung 3"
+        ]
+      },
+      "Wireframe": {
+        "path": "uebungen/3/a3-wireframe.html",
+        "title": "Wireframe Uebung 3",
+        "description": [
+          "Wireframe von Uebung 3"
+        ]
+      }
+    },
+    "Javascript_1": {
+      "Flexbox": {
+        "path": "uebungen/4/function-visualisation",
+        "title": "Funktionen von Uebung 4",
+        "description": [
+          "Function von Uebung 4 - Anzeige nur innerhalb der Konsole"
+        ]
+      }
+    },
+    "Javascript_2": {
+      "Performance": {
+        "path": "uebungen/5/performance.html",
+        "title": "Performance",
+        "description": [
+          "Performance Tests in Javascript"
+        ]
+      },
+      "Rednerliste": {
+        "path": "uebungen/5/performance.html",
+        "title": "Rednerliste",
+        "description": [
+          "Rednerliste in Javascript"
+        ]
+      },
+      "Topsort": {
+        "path": "uebungen/5/topsort.html",
+        "title": "Topsort",
+        "description": [
+          "Topsort in Javascript"
+        ]
+      }
+    },
+    "Javascript_3": {
+      "Ecmascript": {
+        "path": "uebungen/6/ecmascript-functions.html",
+        "title": "Ecmascript",
+        "description": [
+          "Performance Tests in Javascript"
+        ]
+      },
+      "Klammerpaar": {
+        "path": "uebungen/6/klammerpaar.html",
+        "title": "Klammerpaar",
+        "description": [
+          "Berechnung von Klammerpaaren in Javascript"
+        ]
+      },
+      "TopsortSchritt": {
+        "path": "uebungen/6/topSortSchritt.html",
+        "title": "TopsortSchritt",
+        "description": [
+          "TopsortSchritt in Javascript"
+        ]
+      }
+    },
+    "Javascript_4": {
+      "Funktional": {
+        "path": "uebungen/7/functional-programming.html",
+        "title": "Funktional",
+        "description": [
+          "Funktionale Programmierung in Javascript - Ausgabe nur in der Konsole"
+        ]
+      },
+      "Plagiatsresolution": {
+        "path": "uebungen/7/plagiatsresolution.html",
+        "title": "Plagiatsresolution",
+        "description": [
+          "Plagiatsresolution in Javascript - Lösung in der Konsole"
+        ]
+      }
+    },
+    "Javascript_5": {
+      "AsyncAwait": {
+        "path": "uebungen/8/asyncAwait.html",
+        "title": "AsyncAwait",
+        "description": [
+          "Async Await in Javascript - Ausgabe nur in der Konsole"
+        ]
+      },
+      "Promises": {
+        "path": "uebungen/8/promises.html",
+        "title": "Promises",
+        "description": [
+          "Promises in Javascript - Lösung in der Konsole"
+        ]
+      },
+      "WWW_Navi": {
+        "path": "uebungen/8/www_navigator.html",
+        "title": "WWW-Navi",
+        "description": [
+          "WWW Navigator"
+        ]
+      }
+    },
+    "SVG": {
+      "Use_The_Platform": {
+        "path": "uebungen/9/use_the_platform.html",
+        "title": "Use_The_Platform",
+        "description": [
+          "SVG Grafik Use The Platform"
+        ]
+      },
+      "Balkendiagram": {
+        "path": "uebungen/9/balkendiagram.html",
+        "title": "Balkendiagram",
+        "description": [
+          "Balkendiagram von Tagesaktuellen Covid-19 Daten des RKI (7-Tage-Inzidenz)"
+        ]
+      },
+      "Tic_Tac_Toe": {
+        "path": "uebungen/9/www_navigator.html",
+        "title": "Tic_Tac_Toe",
+        "description": [
+          "Interaktive SVG Grafik - Tic Tac Toe"
+        ]
+      }
+    },
+    "TypeScript": {
+      "Textaufgabe": {
+        "path": "uebungen/10/textaufgabe.html",
+        "title": "Textaufgabe",
+        "description": [
+          "Lösung der Textaufgabe zu TypeScript"
+        ]
+      }
+    }
+  }
